ed77a7b0e287c737faccd90fc4e1cd2614fd1b29
Vivelab24 <Vivelab24@portatilrutan27.rutanmedellin.loc>
2016-02-12 17:33:24 -0500

    ejercicios ruby

---
+++ 06Ruby/condicionales.rb
@@ -0,0 +1,48 @@
+#puts "escriba su edad"
+#edad = gets.chomp.to_f
+#edad20 = gets.chomp.to_f
+
+#f edad >= 12
+#	puts"puede entrar a la montaña rusa"
+#elsif edad >=4
+#	puts "puede entrar al gusanito"
+#else 
+ #   puts  "puede entrar al carrusel"
+#end   
+
+ # puts "escriba su dia de nacimiento"
+ # dia = gets.chomp
+ # dia = dia.to_i
+
+ # puts "escriba el numero del mes en que nacio"
+ # mes= gets.chomp
+ # mes =mes.to_i
+
+ # if   mes == 1 && dia<= 21
+ # puts "ud es capricornio"
+	
+
+ # else 
+ #     puts  "la fecha no existe"
+ # end  
+
+
+puts "escriba su mes"
+ dia = gets.chomp
+ dia = dia.to_i
+
+ puts "escriba su dia "
+ mes= gets.chomp
+  mes =mes.to_i 
+
+if   mes == 1 && dia<= 21
+ # puts "ud es capricornio"
+
+	unless   mes == 3 && dia<= 30
+	 puts "ud es  no  espiscis"
+		
+
+	 end  
+end
+
+
---
+++ 06Ruby/ejercicio.rb
@@ -0,0 +1,9 @@
+puts "ingrese su primera nota"
+a= gets.chomp
+puts "ingrese su segunda nota"
+b= gets.chomp
+puts "ingrese su tercera nota"
+c= gets.chomp
+puts "ingrese su cuarta nota"
+d= gets.chomp
+promedio=(a+b+c+d)/4  
