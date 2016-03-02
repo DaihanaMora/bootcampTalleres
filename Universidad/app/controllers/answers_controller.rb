03fafd5e00571912ab9e2cc2b2b440c4afd7739b
Vivelab24 <Vivelab24@portatilrutan27.rutanmedellin.loc>
2016-03-02 17:57:35 -0500

    termina ejercicio universidad

--- Universidad/app/controllers/answers_controller.rb
+++ Universidad/app/controllers/answers_controller.rb
@@ -1,4 +1,44 @@
 class AnswersController < ApplicationController
 
-	
+	def index
+		@answers = Answer.all
+		
+	end
+
+	def edit
+        
+       @answer = Answer.find(params[:id]) 
+       
+    end
+
+    def update
+    	@answer = Answer.find(params[:id])#busco cual es el profe
+        @answer.update(homework_params)
+        redirect_to homeworks_path
+    end
+
+    def new
+    	@answer = Answer.new
+    end
+
+    def create
+		@answer = Answer.new(answer_params)
+		@answer.save#para guardar
+		redirect_to homeworks_path# redireciona la ruta a home#index
+	end
+
+
+    def destroy
+    	@answer = Answer.find(params[:id])
+    	@answer.destroy
+    	redirect_to answers_path
+
+    	
+    end
+
+	def answer_params
+         params.require(:answer).permit(:contenido, :calificacion, :student_id, :homework_id)
+       
+    end
+
 end
--- Universidad/app/controllers/teachers_controller.rb
+++ Universidad/app/controllers/teachers_controller.rb
@@ -4,4 +4,41 @@ class TeachersController < ApplicationController
 		@teachers = Teacher.all
 		
 	end
+
+	def edit
+        
+       @teacher = Teacher.find(params[:id]) 
+       
+    end
+
+    def update
+    	@teacher = Teacher.find(params[:id])#busco cual es el profe
+        @teacher.update(teacher_params)
+        redirect_to teachers_path
+    end
+
+    def new
+    	@teacher = Teacher.new
+    end
+
+    def create
+		@teacher = Teacher.new(teacher_params)
+		@teacher.save#para guardar
+		redirect_to teachers_path# redireciona la ruta a home#index
+	end
+
+
+    def destroy
+    	@teacher = Teacher.find(params[:id])
+    	@teacher.destroy
+    	redirect_to teachers_path
+
+    	
+    end
+
+	def teacher_params
+         params.require(:teacher).permit(:nombre, :apellido, :identificacion, :email)
+       
+    end
+
 end
