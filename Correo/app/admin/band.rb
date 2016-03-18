bbed66407278796b1c4406db2120e5fb72ce276c
Vivelab24 <Vivelab24@portatilrutan27.rutanmedellin.loc>
2016-03-18 17:50:48 -0500

    correo

---
+++ Correo/app/admin/band.rb
@@ -0,0 +1,37 @@
+ActiveAdmin.register Band do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :genero
+index do |bamd|
+	selectable_column
+	column :name
+	actions
+end
+
+    form do |f|
+		f.inputs 'informacion basica' do
+		f.input :name
+		f.input :genero
+
+	end	
+   f.actions
+end
+show do
+		attributes_table do
+		   row :name
+		   row :genero
+
+	end		
+end	
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end
