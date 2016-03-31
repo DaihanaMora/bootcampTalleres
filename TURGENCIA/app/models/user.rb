51d6f2ac73e2491d4b363ec31d2945ac6fe11a17
Vivelab24 <Vivelab24@iMac-de-Vivelab24.local>
2016-03-31 17:59:26 -0500

    agregue carrierwave,linkie las paginas

--- TURGENCIA/app/models/user.rb
+++ TURGENCIA/app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
 	has_many :clinics, through: :lists
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  mount_uploader :avatar, AvatarUploader 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable ,:confirmable
+
+
 end
--- TURGENCIA/db/schema.rb
+++ TURGENCIA/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330215143) do
+ActiveRecord::Schema.define(version: 20160331211710) do
 
   create_table "clinics", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160330215143) do
     t.string   "eps"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
